759c708ed1bfa15d67b2084ee604513182358815
MatheusTurra <matheusfr.318@gmail.com>
2022-07-20 18:42:21 -0300

    Login e registro de usuário

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Api\LoginController;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $login = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        if (!Auth::attempt($login)) {
+            return response(['message' => 'Invalid login credentials.']);
+        }
+
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+        return response(['user' => Auth::user(), 'access_token' => $accessToken]);
+    }
+
+    public function registerUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $hashedPassword = Hash::make($request->password);
+
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $hashedPassword;
+        $user->save();
+    }
+}
