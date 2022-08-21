cabd3d324982a26a835da11104b497e2dcd08e14
MatheusTurra <matheusfr.318@gmail.com>
2022-08-21 20:26:36 -0300

    Salvar ou atualizar usuário em autenticação oauth
    - Classe helper de usuário criada para desacoplar a model da controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,26 +6,31 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 
+use App\Helpers\UserHelper;
+
 class UserController extends Controller
 {
     /**
      * TODO: VERIFICAR SE O EMAIL JÁ EXISTE NO BANCO DE DADOS
+     * TODO: REMOVER A COLUNA NAME DA TABELA USER_ACCESS_TOKENS (E REMOVER TAMBÉM NO INSERT DA USERHELPER)
      */
 
     public function save_token(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $user_validator = Validator::make($request->all(), [
             'email' => 'required|email:rfc,dns|filled',
             'name' => 'required|nullable',
             'access_token' => 'required|filled'
         ]);
 
-        if($validator->fails()) {
+        if($user_validator->fails()) {
             return response()->json([
                 'error' => 'Some request parameters failed at validation',
-                'failed_fields' => $validator->errors()
+                'failed_fields' => $user_validator->errors()
             ], 400);
         }
+
+        UserHelper::save_or_update_user($request->all());
 
         return response()->json([
             'email' => $request->email,
