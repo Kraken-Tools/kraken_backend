79d798ceeedcbca0bec57f0b5cedf9e38c1f18dd
MatheusTurra <matheusfr.318@gmail.com>
2022-09-14 22:54:28 -0300

    Resolver bugs de conversão de moeda e milhar
    - Resolvido bug de milhar ex: 1234 retornava um mil e duzentos e trinta e quatro;
    - Resolvido bug de moeda quando o número era maior do que um milhão.

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use WGenial\NumeroPorExtenso\NumeroPorExtenso;
 
-use App\Services\ToolsService;
+use App\Services\ToolService;
 
 class ToolsController extends Controller
 {
@@ -14,8 +13,7 @@ class ToolsController extends Controller
         $request->merge(['number' => $request->route('number')]);
         $validator = $request->validate(['number' => 'required|numeric']);
 
-        $number_converter = new NumeroPorExtenso;
-        $number_in_full = $number_converter->converter($request->number);
+        $number_in_full = ToolService::getNumberInFull($request->number);
 
         return response()->json(['result' => $number_in_full]);
     }
